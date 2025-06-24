67f37cf79801087132236679d39100b206777294
Opti99333 <opti1298@gmail.com>
2025-06-24 12:33:44 +0300

    Add files via upload

---
+++ framerusercontent.com/sites/7tA95ltjoDrNLfwhnJqfeb/chunk-ATINE7BP.mjs
@@ -0,0 +1,4 @@
+function t(a,e){return{customHTMLBodyStart:`<script>
+  document.body.style.overscrollBehavior = "none";
+<\/script>`,favicon:"https://framerusercontent.com/assets/6b34PhaobwpF4gfBFDcsp81ay8.jpg",robots:"max-image-preview:large",socialImage:"https://framerusercontent.com/assets/aMVC5VVD1E1Piu8y5C5p6CCdpo.png",title:"BLOB CTO"}}export{t as a};
+//# sourceMappingURL=chunk-ATINE7BP.mjs.map
